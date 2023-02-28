60713ac887d20c84e0fbdacbf98ec2ec5bd366b0
webson2020 <nrossettiossa@gmail.com>
2023-02-28 12:03:10 -0300

    index and ProjectAssigments

--- index.ts
+++ index.ts
@@ -7,21 +7,32 @@ import { projects } from './seeders/projects';
 
 
 
-const createUsers = () => {
-    users.map(user => {
-        db.User.create(user)
-    })
-};
+// const createUsers = () => {
+//     users.map(user => {
+//         db.User.create(user)
+//     })
+// };
 
-createUsers();
+// createUsers();
 
 
-const createProjects = () => {
-    projects.map(project => {
-        db.Project.create(project)
-    })
-};
-createProjects();
+// const createProjects = () => {
+//     projects.map(project => {
+//         db.Project.create(project)
+//     })
+// };
+// createProjects();
+
+app.get('/', (req, res) => {
+    db.User.findAll({
+        include: {
+            model: db.ProjectAssignment
+        }
+    }).then((result: object) => res.json(result)).catch((err: object) => console.error(err));
+})
+
+
+//TODO: createProjectsAssigments();
 
 
 db.sequelize.sync().then(() => {
---
+++ seeders/projectassigments.ts
@@ -0,0 +1,14 @@
+export const projectassigments = [
+    {
+        ProjectId: 1,
+        UserId: 'dc45d7f0-8eee-4a46-a9f6-c4c5bb8d9845'
+    },
+    {
+        ProjectId: 2,
+        UserId: '7423feb8-08ef-472d-ab36-28eaa040b8f7'
+    },
+    {
+        ProjectId: 1,
+        UserId: 'dc45d7f0-8eee-4a46-a9f6-c4c5bb8d9845'
+    }
+];
